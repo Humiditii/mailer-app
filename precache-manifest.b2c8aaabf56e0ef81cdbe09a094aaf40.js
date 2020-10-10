01db82fffb0fd487b072a9d8faf9182fcbc0bc33
Babatunde Abdulhameed Olakunle <humiditii45@gmail.com>
2020-10-10 14:17:01 +0000

    Updates

---
+++ precache-manifest.b2c8aaabf56e0ef81cdbe09a094aaf40.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5db88263f0d8ca2c16c5fa59b7de8de",
+    "url": "/mailer-app/index.html"
+  },
+  {
+    "revision": "8f2376eec647de845342",
+    "url": "/mailer-app/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "d062441e111d2ad2e3f6",
+    "url": "/mailer-app/static/css/main.157aab26.chunk.css"
+  },
+  {
+    "revision": "8f2376eec647de845342",
+    "url": "/mailer-app/static/js/2.03d9527a.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/mailer-app/static/js/2.03d9527a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d062441e111d2ad2e3f6",
+    "url": "/mailer-app/static/js/main.6f7aa973.chunk.js"
+  },
+  {
+    "revision": "4d033526bcd27f1801e2",
+    "url": "/mailer-app/static/js/runtime-main.6013026b.js"
+  },
+  {
+    "revision": "3f5ccd9316a1f07dedfdc4c8428f9906",
+    "url": "/mailer-app/static/media/Anonymous.3f5ccd93.jpg"
+  }
+]);
